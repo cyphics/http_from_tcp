a20f442f2040f47e3402a64b11179c387df69918
Thierry Raeber <cyphics@protonmail.com>
2026-07-26 16:19:20 +0200

    1.5

--- main.go
+++ main.go
@@ -1,35 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func check(e error) {
 	if e != nil {
-		os.Exit(0)
+		if errors.Is(e, io.EOF) {
+			// fmt.Print("EOF")
+		} else {
+			fmt.Print(e)
+			os.Exit(1)
+		}
 	}
 }
 
-func main() {
-	f, err := os.Open("message.txt")
-	check(err)
-	const ChunkLengh = 8
-
+func read_file(f io.ReadCloser, str_chan chan<- string) {
+	defer f.Close()
+	defer close(str_chan)
+	const ChunkLenght = 8
 	var current_line = ""
 	
-	var BytesRead = ChunkLengh
-	chunk := make([]byte, ChunkLengh)
-	for BytesRead == ChunkLengh { 
+	chunk := make([]byte, ChunkLenght)
+	for { 
 		BytesRead, err := f.Read(chunk)
 		check(err)
 		current_line += string(chunk[:BytesRead])
 		var split = strings.Split(current_line, "\n")
 		if len(split) > 1 {
-			fmt.Printf("read: %s\n", split[0])
+			str_chan<-split[0]
+			// fmt.Printf("load %s\n", split[0])
 			current_line = split[1]
 		}
-		// fmt.Println(len(split))
+
+		if BytesRead < ChunkLenght {
+			break
+		}
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	var chan_str = make(chan string, 1)
+	go read_file(f, chan_str)
+	return chan_str
+}
+
+func main() {
+	f, err := os.Open("message.txt")
+	check(err)
+	var lines = getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
