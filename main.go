610556c986afddfcffcac9d08b6d8f97c852566a
Thierry Raeber <cyphics@protonmail.com>
2026-07-26 16:59:58 +0200

    copy solution

--- main.go
+++ main.go
@@ -4,63 +4,65 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
-	"os"
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		if errors.Is(e, io.EOF) {
-		} else {
-			fmt.Printf("ERROR: %s", e)
-			os.Exit(1)
-		}
+const port = ":42069"
+
+func main() {
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("error listening for TCP traffic: %s\n", err.Error())
 	}
-}
+	defer listener.Close()
 
-func read_file(f io.ReadCloser, str_chan chan<- string) {
-	defer f.Close()
-	defer close(str_chan)
-	const ChunkLenght = 8
-	var current_line = ""
-	
-	chunk := make([]byte, ChunkLenght)
-	for { 
-		BytesRead, err := f.Read(chunk)
-		check(err)
-		current_line += string(chunk[:BytesRead])
-		var split = strings.Split(current_line, "\n")
-		if len(split) > 1 {
-			str_chan<-split[0]
-			current_line = split[1]
+	fmt.Println("Listening for TCP traffic on", port)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("error: %s\n", err.Error())
 		}
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		if BytesRead < ChunkLenght {
-			break
+		linesChan := getLinesChannel(conn)
+
+		for line := range linesChan {
+			fmt.Println(line)
 		}
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 	}
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
-	var chan_str = make(chan string, 1)
-	go read_file(f, chan_str)
-	return chan_str
-}
-
-func main() {
-	var listener, err = net.Listen("tcp", "127.0.0.1:42069")
-	check(err)
-	defer listener.Close()
-
-	for {
-		var connection, err = listener.Accept()
-		fmt.Println("Connecton accepted.")
-		check(err)
-		var lines = getLinesChannel(connection)
-		for line := range lines {
-			fmt.Println(line)
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		fmt.Println("Connection closed.")
-	}
+	}()
+	return lines
 }
+
